f60a1d4f038c469b8866e3280dd218b10592034b
Vacant Fanatic <agill084@gmail.com>
2014-04-01 13:56:50 +0000

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VAcationCalendar::Application.config.secret_key_base = '856933e85271c8568950cfe6354663aea46fa8f432d662fa1770b06e7141543240a7f557050656523280c0fa35aece082ceb82b993c3ca483b77c01483003e12'
