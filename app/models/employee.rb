1925e9b5186c36df3ed5cfb7d416105f9813271e
Vacant Fanatic <agill084@gmail.com>
2014-04-01 19:20:44 +0000

    Spork & Guard Config

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,16 @@
 class Employee < ActiveRecord::Base
   has_many :Events
+  
+  attr_accessor :name, :email
+
+  def initialize(attributes = {})
+    @name  = attributes[:name]
+    @email = attributes[:email]
+  end
+
+  def formatted_email
+    "#{@name} <#{@email}>"
+  end
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 VacationCalendar::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/help"
+  get "static_pages/about"
   resources :events
 
   resources :employees
